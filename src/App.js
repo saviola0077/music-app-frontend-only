35f4b4fbfb2d12b353fd10f2263b589fa2af3e28
saviola0077 <69299914+saviola0077@users.noreply.github.com>
2025-03-02 18:09:24 +0000

    had some bugs that had to do with my img_src after the creating props for PlayerDetails. has been fixed a series of debugging

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ function App() {
     {
       title: "Joy is coming",
       artist: "Fido",
-      img_src: "./images/fido.jpg"
-      src: "./music/Fido-Joy-Is-Coming-(TrendyBeatz.com).mp3"
+      img_src: "./images/fido.jpg",
+      src: "./music/Joy is coming.mp3",
     },
     {
       title: "Military",
       artist: "Asake",
-      img_src: "./images/asake.jpg"
+      img_src: "./images/asake.jpg",
       src: "./music/Asake_-_Military.mp3"
     },
     {
       title: "Jay Jay",
       artist: "Ruger",
-      img_src: "./images/Ruger_(Singer).jpg"
-      src: "./music/Ruger-Jay-Jay-(TrendyBeatz.com).mp3"
+      img_src: "./images/Ruger_(Singer).jpg",
+      src: "./music/ruger jay jay.mp3"
     },
     {
       title: "Safe Haven",
       artist: "Omahlay",
-      img_src: "./images/omahlay.jpg"
-      src: "./music/7310_Omah Lay - Safe Haven SHOW2BABI.COM.mp3"
+      img_src: "./images/omahlay.jpg",
+      src: "./music/safe haven.mp3"
     }
   ]);
 
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import PlayerDetails from './PlayerDetails';
 
-function Player() {
+function Player(props) {
     return (
         <div className="c-player">
             <audio></audio>
             <h4>Playing now</h4>
-            {/* DETAILS*/}
+            <PlayerDetails song={props.song} />
             {/* CONTROLS*/}
-            <p><strong>Next up:</strong>Military by Asake</p>
+            <p><strong>Next up:</strong>{props.nextSong.title} by {props.nextSong.artist}</p>
         </div>
     )
 }
