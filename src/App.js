6f5ef5d0a058e64004e806be245b88582115faba
saviola0077 <69299914+saviola0077@users.noreply.github.com>
2025-03-02 16:31:47 +0000

    created components : Player, PlayerControl and PlayerDetails

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import {useState} from 'react';
 function App() {
   const [songs, setSongs] = useState([
     {
-    title: "Joy is coming",
-    artist: "Fido",
-    img_src: ".\images\fido.jpg"
-    src: "\music\Fido-Joy-Is-Coming-(TrendyBeatz.com).mp3"
-
+      title: "Joy is coming",
+      artist: "Fido",
+      img_src: "./images/fido.jpg"
+      src: "./music/Fido-Joy-Is-Coming-(TrendyBeatz.com).mp3"
+    },
+    {
+      title: "Military",
+      artist: "Asake",
+      img_src: "./images/asake.jpg"
+      src: "./music/Asake_-_Military.mp3"
+    },
+    {
+      title: "Jay Jay",
+      artist: "Ruger",
+      img_src: "./images/Ruger_(Singer).jpg"
+      src: "./music/Ruger-Jay-Jay-(TrendyBeatz.com).mp3"
+    },
+    {
+      title: "Safe Haven",
+      artist: "Omahlay",
+      img_src: "./images/omahlay.jpg"
+      src: "./music/7310_Omah Lay - Safe Haven SHOW2BABI.COM.mp3"
+    },
   ])
   return (
     <div className="App">
