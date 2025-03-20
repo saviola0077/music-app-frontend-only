7dab76ba7ba9ea00fab1dd7404f5dd00c2e3f5f4
saviola0077 <69299914+saviola0077@users.noreply.github.com>
2025-03-20 21:32:30 +0000

    updated fontawesome icon to show if music is playing or paused

--- src/components/PlayerControls.js
+++ src/components/PlayerControls.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause, faForward,faBackward} from '@fortawesome/free-solid-svg-icons';
 
-function PlayerControls() {
+function PlayerControls(props) {
     return (
         <div className="c-player--controls">
             <button className="skip-btn">
                 <FontAwesomeIcon icon={faBackward} />
             </button>
-            <button className="play-btn">
-                <FontAwesomeIcon icon={faPlay} />
+            <button className="play-btn" onClick={() => props.setIsPlaying(!props.isPlaying)}>
+                <FontAwesomeIcon icon={props.isPlaying ? faPause : faPlay} />
             </button>
             <button className="skip-btn">
                 <FontAwesomeIcon icon={faForward} />
