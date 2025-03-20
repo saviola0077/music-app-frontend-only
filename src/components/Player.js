9152c1ef9818c15b6dde2b80088a01c6ef93ea27
saviola0077 <69299914+saviola0077@users.noreply.github.com>
2025-03-20 20:56:49 +0000

    created and configured the SkipSong function for the forward and backward button

--- src/components/Player.js
+++ src/components/Player.js
@@ -6,6 +6,41 @@ function Player(props) {
     const audioEl = useRef(null);
     const [isPlaying, setIsPlaying] = useState(false);
 
+    useEffect (() => {
+        if (isPlaying) {
+            audioEl.current.play();
+        } else {
+            audioEl.current.pause();
+        }
+    });
+
+    const SkipSong = (forward = true) => {
+        if (forward) {
+            props.setCurrentSongIndex(() => {
+                let temp = props.currentSongIndex;
+                temp++;
+
+                if (temp > props.songs.length - 1) {
+                    temp = 0;
+                }
+
+                return temp;
+            });
+        } else {
+            props.setCurrentSongIndex(() => {
+                let temp = props.currentSongIndex;
+                temp--;
+
+                if (temp < 0) {
+                     temp = props.songs.length - 1;
+                }
+
+                return temp;
+            });
+
+        }
+    }
+
 
     return (
         <div className="c-player">
