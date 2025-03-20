b38944b61fe45f65b9bbcd9dd8d53286fb7ea03a
saviola0077 <69299914+saviola0077@users.noreply.github.com>
2025-03-20 07:31:09 +0000

    created another state to check if the song is playing

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,9 +3,13 @@ import PlayerDetails from './PlayerDetails';
 import PlayerControls from './PlayerControls';
 
 function Player(props) {
+    const audioEl = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+
     return (
         <div className="c-player">
-            <audio></audio>
+            <audio ref={audioEl}></audio>
             <h4>Playing now</h4>
             <PlayerDetails song={props.song} />
             <PlayerControls />
